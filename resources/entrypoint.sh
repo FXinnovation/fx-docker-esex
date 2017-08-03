6a7e5afc8e83ac99d57c732c0f24f0cafa6fe835
Zopanix <zopanix@gmail.com>
2017-08-03 14:31:16 -0400

    Making minor adjustments

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -3,7 +3,7 @@ set -e -x
 
 # Apply configuration of snapshots on es cluster
 curl -q \
-  -XPUT "${ESEX_ES_HOST}:${ESEX_ES_PORT}/_snapshot/snapshot-repository" \
+  -XPUT "${ESEX_ES_HOST}:${ESEX_ES_PORT}/_snapshot/${ESEX_S3_BUCKET_NAME}" \
   -d "{\"type\":\"s3\",\"settings\":{\"bucket\":\"${ESEX_S3_BUCKET_NAME}\",\"region\":\"${ESEX_S3_BUCKET_REGION}\",\"role_arn\":\"${ESEX_S3_ROLE_ARN}\"}}"
 
 curl -q \
@@ -17,7 +17,7 @@ LIMIT_DATE=$(date -d "${ESEX_ES_RETENTION_DAYS} days ago" +"%Y.%m.%d")
 INDICE_LIST=$(curl -q \
                 -X GET \
                 -H "Accept: application/json" \
-                "${ESEX_ES_HOST}:${ESEX_ES_PORT}/_cat/indices?format=json" | jq -r '.[].index' |sort )
+                "${ESEX_ES_HOST}:${ESEX_ES_PORT}/_cat/indices?format=json" | jq -r '.[].index' |sort | grep -v kibana)
 
 # For each indice
 for INDICE in ${INDICE_LIST}
@@ -27,32 +27,31 @@ do
   INDICE_DATE=$(echo ${INDICE} | awk -F "-" '{ print $NF }')
   # If the indice date is smaller then the limit data
   if [ "${INDICE_DATE}" \< "${LIMIT_DATE}" ]; then
-    if [[ "$INDICE_DATE" == *"kibana"* ]]; then
-      echo "Indice '${INDICE}' is not eligible for snapshot!"
-    else
-      echo "Indice: '${INDICE}' is going to be snapshoted"
-      # Asking for export
-      curl -q \
-        -X PUT \
-        "${ESEX_ES_HOST}:${ESEX_ES_PORT}/_snapshot/${ESEX_S3_BUCKET_NAME}/${INDICE}" \
-        -d "{\"indices\":\"${INDICE}\",\"include_global_state\":false,\"ignore_unavailable\":false}"
-      # Waiting for export to be done
-      SNAPSHOT_ONGOING="TRUE"
-      while [ "${SNAPSHOT_ONGOING}" == "TRUE" ]
-      do
-        echo "Waiting for snapshot to finish"
-        curl -q \
-          -X GET \
-          "${ESEX_ES_HOST}:${ESEX_ES_PORT}/_snapshot/${ESEX_S3_BUCKET_NAME}/${INDICE}?format=json"
-        # TODO Parse result HERE
+    echo "Indice: '${INDICE}' is going to be snapshoted"
+    # Asking for export
+    curl -q \
+      -X PUT \
+      "${ESEX_ES_HOST}:${ESEX_ES_PORT}/_snapshot/${ESEX_S3_BUCKET_NAME}/${INDICE}" \
+      -d "{\"indices\":\"${INDICE}\",\"include_global_state\":false,\"ignore_unavailable\":false}"
+    # Waiting for export to be done
+    SNAPSHOT_ONGOING="TRUE"
+    while [ "${SNAPSHOT_ONGOING}" == "TRUE" ]
+    do
+      echo "Waiting for snapshot to finish"
+      STATE=$(curl -q \
+        -X GET \
+        "${ESEX_ES_HOST}:${ESEX_ES_PORT}/_snapshot/${ESEX_S3_BUCKET_NAME}/${INDICE}?format=json | jq -r '.snapshots[].state'"
+      # TODO Parse result HERE
+      if [ "${STATE}" == "SUCCESS" ]; then
         SNAPSHOT_ONGOING="FALSE"
+      else
         sleep 10
-      done
-      # Asking for deletion
-      #curl -q \
-      #  -X DELETE \
-      #"${ESEX_ES_HOST}:${ESEX_ES_PORT}/${INDICE}?format=json"
-    fi
+      fi
+    done
+    # Asking for deletion
+    curl -q \
+      -X DELETE \
+    "${ESEX_ES_HOST}:${ESEX_ES_PORT}/${INDICE}?format=json"
   else
     echo "Indice: '${INDICE}' is to recent for export, skipping."
   fi
